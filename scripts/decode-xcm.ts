762d9dbc85c216d5786ea984bc983caac22524b1
gorka <gorka.irazoki@gmail.com>
2022-03-30 17:06:16 +0200

    add decoder for a block, and para, looking for a message in relay

---
+++ scripts/decode-xcm.ts
@@ -0,0 +1,50 @@
+// Import
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import yargs from 'yargs';
+
+const args = yargs.options({
+    'relay-ws-provider': {type: 'string', demandOption: true, alias: 'wr'},
+    'block-number': {type: 'number', demandOption: true, alias: 'b'},
+    'para-id': {type: 'number', demandOption: true, alias: 'p'},
+
+  }).argv;
+ 
+// Construct
+const relayProvider = new WsProvider(args['relay-ws-provider']);
+
+async function main () {
+    const relayApi = await ApiPromise.create({ provider: relayProvider });
+    const para =  relayApi.createType("ParaId", args['para-id']) as any;
+
+    // returns Hash
+    const blockHash = await relayApi.rpc.chain.getBlockHash(args['block-number']) as Uint8Array;
+    // returns SignedBlock
+    const signedBlock = await relayApi.rpc.chain.getBlock(blockHash) as any;
+    
+    // the hash for each extrinsic in the block
+    signedBlock.block.extrinsics.forEach((ex, index) => {
+        // Parachain Inherent enter.
+        // Probably needs to be mapped to pallet index too
+        if (ex.method._meta['name'] == 'enter') {
+            ex.method.args[0].backedCandidates.forEach((candidate,index) => {
+                if (candidate.candidate.descriptor.paraId.eq(para) == true) {
+                    candidate.candidate.commitments['upwardMessages'].forEach((message) => {
+                        // We recover all instructions
+                        let instructions = relayApi.createType("XcmVersionedXcm", message) as any;
+
+                        // We check whether the instruction is a WithdrawAsset
+                        instructions.asV2.forEach((instruction) => {
+                            if (instruction.isWithdrawAsset) {
+                                console.log("We have a withdraw asset")
+                            }
+                        });
+                    });
+                } 
+            });
+
+        }
+    });
+}
+
+
+main().catch(console.error).finally(() => process.exit());

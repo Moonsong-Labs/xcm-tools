e1018c56d9bd582a26ea2a180a562691091aaac5
Albertov19 <albertoviera@gmail.com>
2022-03-31 13:05:49 +0200

    comments and finished para

--- scripts/decode-xcm-para.ts
+++ scripts/decode-xcm-para.ts
@@ -1,56 +1,87 @@
 // Import
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import yargs from 'yargs';
-import { ParaId } from '@polkadot/types/interfaces';
 
 const args = yargs.options({
-    'para-ws-provider': {type: 'string', demandOption: true, alias: 'wr'},
-    'block-number': {type: 'number', demandOption: true, alias: 'b'},
-    'channel': {choices: ['dmp', 'hrmp'], demandOption: true, alias: 'channel'},
-    'para-id': {type: 'number', demandOption: false, alias: 'p'},
+    'para-ws-provider': { type: 'string', demandOption: true, alias: 'wr' },
+    'block-number': { type: 'number', demandOption: true, alias: 'b' },
+    channel: { choices: ['dmp', 'hrmp'], demandOption: true, alias: 'channel' },
+    'para-id': { type: 'number', demandOption: false, alias: 'p' },
+}).argv;
 
-  }).argv;
- 
 // Construct
 const paraProvider = new WsProvider(args['para-ws-provider']);
 
-async function main () {
+async function main() {
+    // Provider
     const paraApi = await ApiPromise.create({ provider: paraProvider });
 
-    // returns Hash
-    const blockHash = await paraApi.rpc.chain.getBlockHash(args['block-number']) as Uint8Array;
-    // returns SignedBlock
-    const signedBlock = await paraApi.rpc.chain.getBlock(blockHash) as any;
-    
+    // Get block hash
+    const blockHash = (await paraApi.rpc.chain.getBlockHash(args['block-number'])) as Uint8Array;
+    // Get block by hash
+    const signedBlock = (await paraApi.rpc.chain.getBlock(blockHash)) as any;
+
     // the hash for each extrinsic in the block
     signedBlock.block.extrinsics.forEach((ex, index) => {
         // Parachain Inherent set validation data.
         // Probably needs to be mapped to pallet index too
         if (ex.method._meta['name'] == 'set_validation_data') {
             if (args['channel'] == 'dmp') {
+                // Check downward messages (from relay chain to parachain)
+                // Go through each message
                 ex.method.args[0].downwardMessages.forEach((message) => {
                     // We recover all instructions
-                    let instructions = paraApi.createType("XcmVersionedXcm", message.msg) as any;
+                    let instructions = paraApi.createType('XcmVersionedXcm', message.msg) as any;
                     if (instructions.isV1) {
-                        if (instructions.asV1.isReserveAssetDeposited) {
-                            console.log("We have a ReserveAssetDeposited asset")
-                        }
-                        console.log(instructions.asV1.toHuman())
-                    }
-                    else if (instructions.isV2) {
+                        // Print V1 Message
+                        console.log(instructions.asV1.toHuman());
+                    } else if (instructions.isV2) {
                         instructions.asV2.forEach((instruction) => {
-                            console.log(instruction.toHuman())
+                            // Print V2 Message
+                            console.log(instruction.toHuman());
+                            if (instruction.isWithdrawAsset) {
+                                console.log('Withdraw Asset Located At');
+                                console.log(instruction.toHuman().WithdrawAsset[0].id);
+                            }
                         });
                     }
                 });
-            }
-            else {
-                const para =  paraApi.createType("ParaId", args['para-id']) as any;
-                console.log("unsupported")
+            } else {
+                // Check hrmp messages (from parachain to parachain)
+                // Types
+                const para = paraApi.createType('ParaId', args['para-id']) as any;
+                ex.method.args[0].horizontalMessages.forEach((messages, paraId) => {
+                    // Filter by the paraId that we want
+                    if (paraId.eq(para)) {
+                        // Go through each message
+                        messages.forEach((message) => {
+                            // First byte is a format version that creates problme when decoding it as XcmVersionedXcm
+                            // We remove it
+                            let instructions = paraApi.createType(
+                                'XcmVersionedXcm',
+                                message.data.slice(1)
+                            ) as any;
+
+                            instructions.asV2.forEach((instruction) => {
+                                // Print V2 Message
+                                console.log(instruction.toHuman());
+                                if (instruction.isReserveAssetDeposited) {
+                                    console.log('Deposit Asset Located At');
+                                    console.log(instruction.toHuman().ReserveAssetDeposited[0].id);
+                                }
+                                if (instruction.isDepositAsset) {
+                                    console.log('Beneficiary Located At');
+                                    console.log(instruction.toHuman().DepositAsset.beneficiary);
+                                }
+                            });
+                        });
+                    }
+                });
             }
         }
     });
 }
 
-
-main().catch(console.error).finally(() => process.exit());
+main()
+    .catch(console.error)
+    .finally(() => process.exit());
--- scripts/decode-xcm-relay.ts
+++ scripts/decode-xcm-relay.ts
@@ -2,49 +2,67 @@
 import { ApiPromise, WsProvider } from '@polkadot/api';
 import yargs from 'yargs';
 import { ParaId } from '@polkadot/types/interfaces';
+import { blake2AsU8a } from '@polkadot/util-crypto';
+import { u8aToHex } from '@polkadot/util';
 
 const args = yargs.options({
-    'relay-ws-provider': {type: 'string', demandOption: true, alias: 'wr'},
-    'block-number': {type: 'number', demandOption: true, alias: 'b'},
-    'para-id': {type: 'number', demandOption: true, alias: 'p'},
+    'relay-ws-provider': { type: 'string', demandOption: true, alias: 'wr' },
+    'block-number': { type: 'number', demandOption: true, alias: 'b' },
+    'para-id': { type: 'number', demandOption: true, alias: 'p' },
+}).argv;
 
-  }).argv;
- 
 // Construct
 const relayProvider = new WsProvider(args['relay-ws-provider']);
 
-async function main () {
+async function main() {
+    // Provider
     const relayApi = await ApiPromise.create({ provider: relayProvider });
-    const para =  relayApi.createType("ParaId", args['para-id']) as any;
 
-    // returns Hash
-    const blockHash = await relayApi.rpc.chain.getBlockHash(args['block-number']) as Uint8Array;
-    // returns SignedBlock
-    const signedBlock = await relayApi.rpc.chain.getBlock(blockHash) as any;
-    
-    // the hash for each extrinsic in the block
-    signedBlock.block.extrinsics.forEach((ex, index) => {
+    // Types
+    const para = relayApi.createType('ParaId', args['para-id']) as any;
+
+    // Get block hash
+    const blockHash = (await relayApi.rpc.chain.getBlockHash(args['block-number'])) as Uint8Array;
+    // Get block by hash
+    const signedBlock = (await relayApi.rpc.chain.getBlock(blockHash)) as any;
+
+    // The hash for each extrinsic in the block
+    signedBlock.block.extrinsics.forEach((extrinsic) => {
         // Parachain Inherent enter.
         // Probably needs to be mapped to pallet index too
-        if (ex.method._meta['name'] == 'enter') {
-            ex.method.args[0].backedCandidates.forEach((candidate,index) => {
+        if (extrinsic.method._meta['name'] == 'enter') {
+            // There might be a backed candidate for different parachains, filter by para ID
+            extrinsic.method.args[0].backedCandidates.forEach((candidate) => {
                 if (candidate.candidate.descriptor.paraId.eq(para) == true) {
+                    // Check upward messages (from parachain to relay chain)
                     candidate.candidate.commitments['upwardMessages'].forEach((message) => {
+                        // Print the Blake2 Hash of the message
+                        console.log(
+                            `The ump.ExecutedUpward event with the hash is in the following block: ${
+                                args['block-number'] + 1
+                            }`
+                        );
+                        console.log(
+                            `Blake2 hash of message is: ${u8aToHex(blake2AsU8a(message))}\n`
+                        );
                         // We recover all instructions
-                        let instructions = relayApi.createType("XcmVersionedXcm", message) as any;
-
-                        // We check whether the instruction is a WithdrawAsset
-                        console.log("instructions are")
+                        let instructions = relayApi.createType('XcmVersionedXcm', message) as any;
+                        console.log('XCM Instructions are:');
+                        // Print V2 instructions
                         instructions.asV2.forEach((instruction) => {
-                            console.log(instruction.toHuman())
+                            console.log(instruction.toHuman());
+                            if (instruction.isWithdrawAsset) {
+                                console.log('Withdraw Asset Located At');
+                                console.log(instruction.toHuman().WithdrawAsset[0].id);
+                            }
                         });
                     });
-                } 
+                }
             });
-
         }
     });
 }
 
-
-main().catch(console.error).finally(() => process.exit());
+main()
+    .catch(console.error)
+    .finally(() => process.exit());

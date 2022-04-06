fc621c7ff1248ce48bfd2d9a33096f68e8ba1818
gorka <gorka.irazoki@gmail.com>
2022-04-06 12:58:28 +0200

    Generic call proposer

---
+++ scripts/generic-call-proposer.ts
@@ -0,0 +1,69 @@
+// Import
+import { ApiPromise, WsProvider } from '@polkadot/api';
+import {hexToU8a} from '@polkadot/util';
+
+import {blake2AsHex} from '@polkadot/util-crypto';
+import yargs from 'yargs';
+import { Keyring } from "@polkadot/api";
+
+const args = yargs.options({
+    'ws-provider': {type: 'string', demandOption: true, alias: 'w'},
+    'generic-call': {type: 'string', demandOption: true, alias: 'call'},
+    'account-priv-key': {type: 'string', demandOption: false, alias: 'account'},
+    'send-preimage-hash': {type: 'boolean', demandOption: false, alias: 'h'},
+    'send-proposal-as': {choices: ['democracy', 'council-external', 'sudo'], demandOption: false, alias: 's'},
+    'collective-threshold': {type: 'number', demandOption: false, alias: 'c'},
+    'at-block': {type: 'number', demandOption: false},
+  }).argv;
+ 
+// Construct
+const wsProvider = new WsProvider(args['ws-provider']);
+
+async function main () {
+    const api = await ApiPromise.create({ provider: wsProvider });
+    const collectiveThreshold = (args['collective-threshold']) ? args['collective-threshold'] :1;
+
+    const proposalAmount = await api.consts.democracy.minimumDeposit as any;
+
+    const keyring = new Keyring({ type: "ethereum" });
+
+    // Necessary to be able to submit and execute
+    let call = api.createType('Call', hexToU8a(args['generic-call'])) as any;
+    let extrinsic = api.createType('GenericExtrinsicV4', call) as any;
+
+    const toPropose = args['at-block'] ? 
+        api.tx.scheduler.schedule(args["at-block"], null, 0, {Value: extrinsic}) :
+        extrinsic;
+
+    console.log(toPropose)
+    const account =  await keyring.addFromUri(args['account-priv-key'], null, "ethereum");
+    const { nonce: rawNonce, data: balance } = await api.query.system.account(account.address) as any;
+    let nonce = BigInt(rawNonce.toString());
+
+    // We just prepare the proposals
+    let encodedProposal = toPropose?.method.toHex() || "";
+    let encodedHash = blake2AsHex(encodedProposal);
+    console.log("Encoded proposal hash for complete is %s", encodedHash);
+    console.log("Encoded length %d", encodedProposal.length);
+
+    if (args["send-preimage-hash"]) {
+        await api.tx.democracy
+        .notePreimage(encodedProposal)
+        .signAndSend(account, { nonce: nonce++ })
+    }
+
+    if (args["send-proposal-as"] == 'democracy') {
+        await api.tx.democracy
+        .propose(encodedHash, proposalAmount)
+        .signAndSend(account, { nonce: nonce++ });
+    }
+    else if (args["send-proposal-as"] == 'council-external') {
+        let external =  api.tx.democracy.externalProposeMajority(encodedHash)
+        
+        await api.tx.councilCollective
+        .propose(collectiveThreshold, external, external.length)
+        .signAndSend(account, { nonce: nonce++ });
+    }
+}
+
+main().catch(console.error).finally(() => process.exit());

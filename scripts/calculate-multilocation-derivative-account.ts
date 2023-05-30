c4a0828950a8315ff09fac4cefd77ba0f85c478e
Albertov19 <albertoviera@gmail.com>
2023-05-30 12:04:54 +0200

    added new standard

--- scripts/calculate-multilocation-derivative-account.ts
+++ scripts/calculate-multilocation-derivative-account.ts
@@ -1,94 +1,97 @@
-import { ApiPromise, WsProvider } from "@polkadot/api";
-import { u8aToHex } from "@polkadot/util";
-import { decodeAddress } from "@polkadot/util-crypto";
+import { u8aToHex, hexToU8a, bnToU8a } from "@polkadot/util";
+import { decodeAddress, blake2AsU8a } from "@polkadot/util-crypto";
 import yargs from "yargs";
-import { MultiLocation } from "@polkadot/types/interfaces";
-import { getXCMVersion } from "./helpers/get-xcm-version";
 import "@moonbeam-network/api-augment";
 
 const args = yargs.options({
-  "parachain-ws-provider": { type: "string", demandOption: true, alias: "w" }, //Target WS Provider
   address: { type: "string", demandOption: true, alias: "a" },
-  "para-id": { type: "string", demandOption: false, alias: "p" }, //Origin Parachain ID,
-  named: { type: "string", demandOption: false, alias: "n" }, // Named optional
+  parents: { type: "boolean", demandOption: false, nargs: 0 }, // Parents
+  "para-id": { type: "number", demandOption: false, alias: "p" }, //Origin Parachain ID
 }).argv;
 
-// Construct
-const wsProvider = new WsProvider(args["parachain-ws-provider"]);
+// Prefix for generating alias accounts
+const FOREIGN_CHAIN_PREFIX_PARA_32 = "ForeignChainAliasAccountPrefix_Para32";
+const FOREIGN_CHAIN_PREFIX_PARA_20 = "ForeignChainAliasAccountPrefix_Para20";
+const FOREIGN_CHAIN_PREFIX_RELAY = "ForeignChainAliasAccountPrefix_Relay";
 
 async function main() {
-  // Create Provider and Type
-  const api = await ApiPromise.create({ provider: wsProvider });
-
-  // Get XCM Version and MultiLocation Type
-  const [xcmVersion, xcmType] = await getXCMVersion(api);
-
   // Check Ethereum Address and/or Decode
+  let decodedAddress;
   let address = args["address"];
-  let account;
   const ethAddress = address.length === 42;
 
-  // Handle name
-  let named;
-  if (args["named"]) {
-    switch (args["named"].toLowerCase()) {
-      case "polkadot":
-        named = { polkadot: null };
-        break;
-      case "kusama":
-        named = { kusama: null };
-        break;
-      case "westend":
-        named = { westend: null };
-        break;
-      default:
-        named = xcmVersion == "V3" ? args["named"] : { Named: args["named"] };
-        break;
-    }
-  } else {
-    named = xcmVersion == "V3" ? null : "Any";
-  }
-
-  // Handle address
+  // Decode Address if Needed
   if (!ethAddress) {
-    address = decodeAddress(address);
-    account = { AccountId32: { network: named, id: u8aToHex(address) } };
+    decodedAddress = decodeAddress(address);
   } else {
-    account = { AccountKey20: { network: named, key: address } };
+    decodedAddress = hexToU8a(address);
   }
 
-  // Handle para-id
-  let interior;
-  if (args["para-id"]) {
-    interior = {
-      X2: [{ Parachain: JSON.parse(args["para-id"]) }, account],
-    };
-  } else {
-    interior = {
-      X1: account,
-    };
+  // Get Component to Hash
+  let toHash;
+  let prefix;
+  let parents;
+  let paraID;
+
+  if (args["parents"]) {
+    // Parents is 1
+    parents = 1;
+
+    if (args["para-id"]) {
+      paraID = args["para-id"];
+
+      // Get Right Para Prefix
+      if (ethAddress) {
+        prefix = FOREIGN_CHAIN_PREFIX_PARA_20;
+      } else {
+        prefix = FOREIGN_CHAIN_PREFIX_PARA_32;
+      }
+
+      // Calculate Hash Component
+      toHash = new Uint8Array([
+        ...new TextEncoder().encode(prefix),
+        ...bnToU8a(args["para-id"], { bitLength: 32 }),
+        ...decodedAddress,
+        ...bnToU8a(parents, { bitLength: 8 }),
+      ]);
+    } else {
+      // Parents 1 - No ParaID - Message is from Relay
+      paraID = null;
+      prefix = FOREIGN_CHAIN_PREFIX_RELAY;
+
+      // Calculate Hash Component
+      toHash = new Uint8Array([
+        ...new TextEncoder().encode(prefix),
+        ...decodedAddress,
+        ...bnToU8a(parents, { bitLength: 8 }),
+      ]);
+    }
+  } else if (args["para-id"]) {
+    // Parents is 0 - But ParaID is Given - This is Nested Parachain in Parachain
+    parents = 0;
+    paraID = args["para-id"];
+
+    if (ethAddress) {
+      prefix = FOREIGN_CHAIN_PREFIX_PARA_20;
+    } else {
+      prefix = FOREIGN_CHAIN_PREFIX_PARA_32;
+    }
+
+    // Calculate Hash Component
+    toHash = new Uint8Array([
+      ...new TextEncoder().encode(prefix),
+      ...bnToU8a(args["para-id"], { bitLength: 32 }),
+      ...decodedAddress,
+      ...bnToU8a(parents, { bitLength: 8 }),
+    ]);
   }
 
-  const multilocation: MultiLocation = api.createType(
-    xcmType,
-    JSON.parse(
-      JSON.stringify({
-        parents: 1,
-        interior: interior,
-      })
-    )
+  console.log(
+    `Remote Origin calculated as ${prefix} - Account ${address} - Parents ${args["parents"]} - ParaID ${paraID}`
   );
 
-  console.log("Multilocation for calculation", multilocation.toString());
-
-  const toHash = new Uint8Array([
-    ...new Uint8Array([32]),
-    ...new TextEncoder().encode("multiloc"),
-    ...multilocation.toU8a(),
-  ]);
-
-  const DescendOriginAddress32 = u8aToHex(api.registry.hash(toHash).slice(0, 32));
-  const DescendOriginAddress20 = u8aToHex(api.registry.hash(toHash).slice(0, 20));
+  const DescendOriginAddress32 = u8aToHex(blake2AsU8a(toHash).slice(0, 32));
+  const DescendOriginAddress20 = u8aToHex(blake2AsU8a(toHash).slice(0, 20));
 
   console.log("32 byte address is %s", DescendOriginAddress32);
   console.log("20 byte address is %s", DescendOriginAddress20);
